7163be3746fae4048a1ad835096b62e827b647ed
Shanthi Pendleton <shanthi.pendleton@gmail.com>
2016-09-28 11:29:45 +0100

    Authentication token for users

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,15 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  validates :auth_token, uniqueness: true
+
+  before_create :generate_authentication_token
+
+  def generate_authentication_token
+    loop do
+      self.auth_token = Devise.friendly_token
+      break unless self.class.exists?(auth_token: auth_token)
+    end
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe User do
-  describe '#create' do
+  describe '#save' do
     context 'with valid info' do
       it 'succeeds' do
         user = build(:user)
@@ -16,6 +16,25 @@ RSpec.describe User do
         user.password_confirmation = 'barry'
 
         expect(user.save).to be false
+      end
+    end
+  end
+
+  describe '#generate_authentication_token' do
+    context 'without a token set' do
+      it 'generates a unique token' do
+        allow(Devise).to receive(:friendly_token).and_return('auniquetoken123')
+        user = create :user
+        expect(user.auth_token).to eq 'auniquetoken123'
+      end
+    end
+
+    context 'when user exists' do
+      it 'generates a different token' do
+        existing_user = create :user
+        user = create :user
+
+        expect(user.auth_token).not_to eq existing_user.auth_token
       end
     end
   end
