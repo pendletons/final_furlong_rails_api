b601d09cd825be02c31c9fd6a902dda65336e536
Shanthi Pendleton <shanthi.pendleton@gmail.com>
2017-04-23 10:38:24 +0100

    Update protect_from_forgery

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
-  protect_from_forgery unless: -> { request.format.json? }
+  protect_from_forgery with: :exception, unless: -> { request.format.json? }
 
   before_action :authenticate
 
