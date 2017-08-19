ce04c053b83c40af56ccd054776842df1b4c10f9
Shanthi Pendleton <shanthi.pendleton@gmail.com>
2017-08-19 10:18:08 +0100

    Rubocop tweaks

--- spec/shared_examples/api_contexts.rb
+++ spec/shared_examples/api_contexts.rb
@@ -1,42 +1,42 @@
 # frozen_string_literal: true
 
-RSpec.shared_context "a failed create" do
+RSpec.shared_examples "a failed create" do
   it "returns an unprocessable entity (422) status code" do
     expect(response.status).to eq(422)
   end
 end
 
-RSpec.shared_context "a response with nested errors" do
+RSpec.shared_examples "a response with nested errors" do
   it "returns the error messages" do
     expect(json_response[:errors][:error]).to eq(error)
   end
 end
 
-RSpec.shared_context "a response with errors" do
+RSpec.shared_examples "a response with errors" do
   it "returns the error messages" do
     expect(json_response[:errors]).to eq(errors)
   end
 end
 
-RSpec.shared_context "a response with an error" do
+RSpec.shared_examples "a response with an error" do
   it "returns the error message" do
     expect(json_response[:error]).to eq(error)
   end
 end
 
-RSpec.shared_context "a show request with a root" do |root|
+RSpec.shared_examples "a show request with a root" do |root|
   it "returns the specified item" do
     expect(json_response[root][:id]).to eq(id)
   end
 end
 
-RSpec.shared_context "a show request" do
+RSpec.shared_examples "a show request" do
   it "returns the specified item" do
     expect(json_response[:id]).to eq(id)
   end
 end
 
-RSpec.shared_context "a successful request" do
+RSpec.shared_examples "a successful request" do
   it "returns an OK (200) status code" do
     expect(response.status).to eq(200)
   end
@@ -47,6 +47,11 @@ RSpec.shared_examples "an authenticated request" do
     path
 
     expect(response.status).to eq 401
+  end
+
+  it "returns acccess denied error" do
+    path
+
     expect(json_error).to eq "Access denied"
   end
 end
