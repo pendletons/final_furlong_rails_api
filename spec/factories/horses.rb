6f26309a5fe7aee49fcb751df508035218f7ab34
Shanthi Pendleton <shanthi.pendleton@gmail.com>
2017-09-20 21:46:11 +0100

    Test other horse-related policies

--- spec/factories/horses.rb
+++ spec/factories/horses.rb
@@ -5,11 +5,24 @@ FactoryGirl.define do
     sequence(:name) { |x| "name #{x}" }
     sequence(:game_id)
     status_id 1
+    type "Racehorse"
 
     trait :with_pedigree do
       after(:create) do |horse|
         create(:horse_pedigree, horse: horse)
       end
+    end
+
+    factory :broodmare do
+      type "Broodmare"
+    end
+
+    factory :stallion do
+      type "Stallion"
+    end
+
+    factory :foal do
+      type "Foal"
     end
   end
 end
